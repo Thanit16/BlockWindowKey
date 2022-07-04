06567c2f544f1c7c58d02a2b8da1b27b2de970f5
MSI Modern <MSI Modern@THANIT>
2022-07-04 10:38:27 +0700

    done

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -17,7 +18,7 @@ namespace BlockWindowKey
         private const int WM_KEYDOWN = 0x0100;
         private static HookProc proc = HookCallback;
 
-        private static IntPtr HookCallback(int nCode, IntPtr wParam,IntPtr lParam)
+        private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
         {
             if ((nCode >= 0) && (wParam == (IntPtr)WM_KEYDOWN))
             {
@@ -29,35 +30,44 @@ namespace BlockWindowKey
                     vkCode);
                     return (IntPtr)1;
                 }
-                return CallNextHookEx(hook, nCode, wParam,lParam);
             }
+            return CallNextHookEx(hook, nCode, wParam, lParam);
         }
 
         private static IntPtr hook = IntPtr.Zero;
-        static void Main()
+        public static void Main()
         {
-            //Application.EnableVisualStyles();
-            //Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new Form1());
             hook = SetHook(proc);
             Application.Run();
             UnhookWindowsHookEx(hook);
         }
-        private delegate IntPtr HookProc(int nCode,)
         private static IntPtr SetHook(HookProc proc)
         {
             using (Process curProcess = Process.
             GetCurrentProcess())
-            using (ProcessModule curModule = curProcess.
-            MainModule)
+            using (ProcessModule curModule = curProcess.MainModule)
             {
-                return SetWindowsHookEx(WH_KEYBOARD_LL,proc,GetModuleHandle(curModule.ModuleName),0);
+                return SetWindowsHookEx(WH_KEYBOARD_LL,
+                proc,
+                GetModuleHandle(curModule.ModuleName),
+                0);
             }
         }
 
-        private static IntPtr SetWindowsHookEx(int wH_KEYBOARD_LL, HookProc proc, object p, int v)
-        {
-            throw new NotImplementedException();
-        }
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook,
+            HookProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk,
+        int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        private delegate IntPtr HookProc(int nCode, IntPtr wParam, IntPtr lParam);
     }
 }
